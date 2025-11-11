cea44b6e7d4a56ae4f2e840c0275d5198994f6d1
the-rajat-singh <rs9460365919@gmail.com>
2025-11-11 18:42:16 +0530

    Create variable.tf

---
+++ modules/network/variable.tf
@@ -0,0 +1,33 @@
+variable "tags" {
+  
+}
+
+
+
+variable "location" {
+  
+}
+variable "vnet_name" {
+  
+}
+
+variable "rg_name" {
+  
+}
+
+variable "subnets" {
+  type        = map(string)
+  
+}
+
+variable "vm_count" {
+  
+}
+
+variable "env" {
+  
+}
+
+variable "pipn" {
+  
+}
