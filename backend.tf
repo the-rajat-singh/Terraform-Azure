c9f02a582a97df9c8bf5c01ebe0b46748cc3a11f
the-rajat-singh <rs9460365919@gmail.com>
2025-11-11 18:36:39 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "storage_for_rajat"
-    storage_account_name = "storagetfstateviarajat"
-    container_name       = "terra"
-    key                  = "terraform.tfstate" 
-  }
-}
